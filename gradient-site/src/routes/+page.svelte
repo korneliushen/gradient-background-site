12c54f9ad93f2662f8374e155f2ecb515c39b610
korneliushen <korneliussen06@gmail.com>
2024-08-23 03:52:47 +0200

    Satt opp Svelte med Tailwind

---
+++ gradient-site/src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<main class="flex flex-col min-h-screen items-center justify-center font-serif">
+    <h1 class="text-red-600 text-9xl">Welcome to SvelteKit</h1>
+    <p class="text-5xl">Look who it is</p>
+</main>
